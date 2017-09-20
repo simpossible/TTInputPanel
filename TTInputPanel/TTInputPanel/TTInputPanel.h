98c85843a74503c3fa45a1f24752be48c9450187
simpossible <963571744@git.cloud.tencent.com>
2017-09-20 17:02:45 +0800

    designpic

---
+++ TTInputPanel/TTInputPanel/TTInputPanel.h
@@ -0,0 +1,13 @@
+//
+//  TTInputPanel.h
+//  TTInputPanel
+//
+//  Created by simp on 2017/9/20.
+//  Copyright © 2017年 simp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTInputPanel : UIView
+
+@end
---
+++ TTInputPanel/TTInputPanel/TTInputPanel.m
@@ -0,0 +1,13 @@
+//
+//  TTInputPanel.m
+//  TTInputPanel
+//
+//  Created by simp on 2017/9/20.
+//  Copyright © 2017年 simp. All rights reserved.
+//
+
+#import "TTInputPanel.h"
+
+@implementation TTInputPanel
+
+@end
